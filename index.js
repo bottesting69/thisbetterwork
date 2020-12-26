49c53dc04f5aef69c345b79d703b309dc78803e9
coinfarmbot1@gmail.com <coinfarmbot1@gmail.com>
2020-12-26 14:07:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Start the connection //
+var Client = require("mpp-client");
+var MPP = new Client("ws://www.multiplayerpiano.com:443");
+var defaultRoom = "Void";
+var botName = "It works!";
+MPP.setChannel (defaultRoom);
+MPP.start();
+
+// On connect //
+MPP.on("connect", function() {
+	setTimeout(function() {MPP.sendArray([{m: "userset", set: {name: botName}}]);}, 500);
+});
